4cb1a0ae84d441d9af661adf8d8e2c746f2bb45b
eugabiviana <gabi.viana88@gmail.com>
2021-04-08 17:09:48 -0300

    My first Servlet

---
+++ gerenciador/src/main/java/br/com/java/alura/gerenciador/servlet/OiMundoServlet.java
@@ -0,0 +1,28 @@
+package br.com.java.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//oi: definindo o caminho do requerimento/endereço da pág.
+@WebServlet(urlPatterns = "/oi") //urlPatterns define o nome do Servlet
+public class OiMundoServlet extends HttpServlet{
+	
+	@Override //essa annotation sobrescreve um método
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		 //Em resp.getWriter é definido o que será a resposta. Como nesse caso é texto, getWriter. Se fosse imagem, seria outro get.
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Oi mundo, parabéns! Você escreveu o primeiro Servlets.");
+		out.println("</body>");
+		out.println("</html>");		
+		
+		System.out.println("o servlet OiMundoServlet foi chamado"); //é um teste para verificar que o servlet foi chamdo dentro do Tomcat.
+	}
+}
